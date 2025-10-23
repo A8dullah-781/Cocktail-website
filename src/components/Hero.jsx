a4adef15a800b8c29821bbd92405e5b5c391924d
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2025-10-23 13:46:47 -0700

    ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,8 +47,8 @@ const Hero = () => {
           .to('.right-leaf', {y:200}, 0)
             .to('.left-leaf', {y:-200}, 0)
 
-            const startValue = isMobile? 'top 50%' : 'center 60%'
-            const endValue = isMobile? ' 120% top' : 'bottom top'
+            const startValue = isMobile? 'top 20%' : 'center 60%'
+            const endValue = isMobile? ' -63% top' : 'bottom top'
 
 
        const tl = gsap.timeline({
@@ -57,7 +57,11 @@ const Hero = () => {
                 start: startValue,
                 end:endValue,
                 scrub:true,
-                pin:true
+                pin:true,
+                if (isMobile) {      
+                tl.scrollTrigger.pin = false
+                } 
+                 
             }
         })
 
