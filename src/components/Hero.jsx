e0b9bd1a2469c7bf035d9680cff1db6eb2ee0bb3
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2025-10-23 14:15:42 -0700

    wargye

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,29 +47,23 @@ const Hero = () => {
           .to('.right-leaf', {y:200}, 0)
             .to('.left-leaf', {y:-200}, 0)
 
-            const startValue = isMobile? 'top 20%' : 'center 60%'
-            const endValue = isMobile? ' -63% top' : 'bottom top'
+	const startValue = isMobile ? "top 50%" : "center 60%";
+const endValue = isMobile ? "120% top" : "bottom top";
 
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: videoRef.current,
+    start: startValue,
+    end: endValue,
+    scrub: true,
+    pin: true, 
+  }
+});
 
-       const tl = gsap.timeline({
-            scrollTrigger:{
-                trigger:'video',
-                start: startValue,
-                end:endValue,
-                scrub:true,
-                pin:true,
-                if (isMobile) {      
-                tl.scrollTrigger.pin = false
-                } 
-                 
-            }
-        })
+videoRef.current.onloadedmetadata = () => {
+  tl.to(videoRef.current, { currentTime: videoRef.current.duration * 0.5 });
+};
 
-        videoRef.current.onloadedmetadata = () =>{
-            tl.to(videoRef.current,{
-                currentTime: videoRef.current.duration * 0.5
-            })
-        }
     }, [])
 
   return (
