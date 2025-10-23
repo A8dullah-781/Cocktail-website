1db368f4c95e5501f792b2627ea735ac66746984
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2025-10-23 16:54:35 -0700

    art

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
             .to('.left-leaf', {y:-200}, 0)
 
 	const startValue = isMobile ? "top 50%" : "center 60%";
-    const endValue = isMobile ? "200% top" : "bottom top";
+    const endValue = isMobile ? "235% top" : "bottom top";
     // mobile py orrr lmba leke jany ky lieeee 200% ko orr bhrhaa dein gy
     
 
@@ -63,8 +63,11 @@ const tl = gsap.timeline({
 });
 
 videoRef.current.onloadedmetadata = () => {
-  tl.to(videoRef.current, { currentTime: videoRef.current.duration * 0.5 });
-};
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+}
+
 
     }, [])
 
